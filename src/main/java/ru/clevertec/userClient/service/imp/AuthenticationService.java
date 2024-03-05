377900a0b9b62d0ee9d325167f6acf6bf6d1b11d
prost333 <dimabolshoy333@yandex.by>
2024-03-05 15:11:39 +0300

    ty

--- src/main/java/ru/clevertec/userClient/service/imp/AuthenticationService.java
+++ src/main/java/ru/clevertec/userClient/service/imp/AuthenticationService.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import ru.clevertec.userClient.controller.NewsClient;
 import ru.clevertec.userClient.dto.UserReq;
 import ru.clevertec.userClient.dto.UserResp;
 import ru.clevertec.userClient.entity.User;
